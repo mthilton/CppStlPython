30e39edf2105578842faa28f00f36fc1b2c71266
Matthew Hilton <mthilton@ucsc.edu>
2021-08-27 08:57:15 -0700

    Updated setup to include proper license classifiers. Also edited the links for style reason

--- setup.py
+++ setup.py
@@ -4,23 +4,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CppStlPython-mthilton",
+    name="CppStlPython",
     version="0.1.0",
-    author="Matthew Hilton",
+    author="@mthilton",
     author_email="redacted@example.com",
     description="A port of the C++ Standard Template Library to Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mthilton/CppStlPython",
     project_urls={
+        "Source": "https://github.com/mthilton/CppStlPython"
         "Bug Tracker": "https://github.com/mthilton/CppStlPython/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "CppStlPython"},
     packages=setuptools.find_packages(where="CppStlPython"),
     python_requires=">=3.5",
+    license='GPL-3.0'
 )
