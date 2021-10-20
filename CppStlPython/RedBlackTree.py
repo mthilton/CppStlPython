d501ba50919b683f8a5460e0df149f19dd4574a4
Matthew Hilton <mthilton@ucsc.edu>
2021-10-20 15:58:41 -0700

    Added comments

--- CppStlPython/RedBlackTree.py
+++ CppStlPython/RedBlackTree.py
@@ -20,19 +20,41 @@ from typing import Any
 
 
 class Color(Enum):
+
+    '''
+    Enum for the colors of the tree.
+    '''
     Black = 0
     Red = 1
 
 
 class RBNode:
 
+    '''
+    Node Class for each node in the tree. Contians the value, color, and children of the node.
+    '''
+
     def __init__(self, _v: Any = None, _c: Color = Color.Red, _l: 'RBNode' = None, _r: 'RBNode' = None) -> None:
+        '''
+        Node Constrcutor. By default, this constructor will generate an empty node colored red with nil children.
+
+        Parameters:
+
+            _v: Value of the node, can take any type. Type checking is done at the tree level. By default, val is
+                None
+            _c: Color of the node. By default, color is red.
+            _l: Left child of the node. By default, the left child is None
+            _r: Right child of the node. By default, the right child is None
+        '''
         self.__val = _v
         self.__color = _c
         self.__left = _l
         self.__right = _r
 
     def __str__(self) -> str:
+        '''
+        overloads the str(RBNode) function. Will return a string representation of RBNode
+        '''
         str_repr = "Value: "
         str_repr += str(self.__val) if self.__val is not None else "None"
         str_repr += "\nColor: "
@@ -46,25 +68,55 @@ class RBNode:
         return str_repr
 
     def get_val(self) -> Any:
+        '''
+        Value getter method for RBNode. Returns the value of the node.
+        '''
         return self.__val
 
     def get_color(self) -> Color:
+        '''
+        Color getter method for RBNode. Returns the color of the node.
+        '''
         return self.__color
 
     def get_left(self) -> 'RBNode':
+        '''
+        Left getter method for RBNode. Returns the left child of Node, can be None.
+        '''
         return self.__left
 
     def get_right(self) -> 'RBNode':
+        '''
+        Right getter method for RBNode. Returns the right child of Node, can be None.
+        '''
         return self.__right
 
     def set_val(self, _v: Any) -> None:
+        '''
+        Value setter method for RBNode. Modifies the value of the node.
+        '''
         self.__val = _v
 
     def set_color(self, _c: Color) -> None:
+        '''
+        Color setter method for RBNode. Modifies the color of the node.
+        '''
         self.__color = _c
 
     def set_left(self, _l: 'RBNode') -> None:
+        '''
+        Left setter method for RBNode. Modifies the left child of Node, can be None.
+        '''
         self.__left = _l
 
     def set_right(self, _r: 'RBNode') -> None:
+        '''
+        Right setter method for RBNode. Modifies the right child of Node, can be None.
+        '''
         self.__right = _r
+
+
+class RedBlackTree:
+
+    def init(self, _r: RBNode = None) -> None:
+        pass
